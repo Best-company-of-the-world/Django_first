588108121de3b96af2f9a2aac70508a859b0f76e
mendelwsys <mendelwsys@gmail.com>
2023-05-27 16:18:49 +0300

    My Commit for idea

--- coool/booot/views.py
+++ coool/booot/views.py
@@ -1,11 +1,16 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Projects
 from django.http import FileResponse
 from random import randint
 import time
+import os
+
 def send_file(response):
 
+
+    print(os.getcwd())
     img = open('coool/booot/images/booot/images/photo_2023-04-13_10-03-09.jpg', 'rb')
 
     response = FileResponse(img)
@@ -15,19 +20,15 @@ def send_file(response):
 def index(request):
     image = Projects.objects.all()
     way = randint(1,4)
-    match way:
-        case 1:
-            time.sleep(randint(1,10)) 
-            response = HttpResponse(content_type="image/jpeg")
-            return send_file(response)   
-        case 2:
-            response = HttpResponse(content_type="image/jpeg")
-            return send_file(response)
-        case 3:
-            return render(request, 'booot/index.html', {'images' : image})
-        case 4:
-            return render(request, 'booot/index2.html')
-    # return send_file(response)
-    # return render(request, 'booot/index.html', {'images' : image})
-    # return render(request, 'booot/images/booot/imagesphoto_2023-04-13_10-03-09.jpg')
+    if way == 1:
+        time.sleep(randint(1,10))
+        response = HttpResponse(content_type="image/jpeg")
+        return send_file(response)
+    elif way == 2:
+        response = HttpResponse(content_type="image/jpeg")
+        return send_file(response)
+    elif way == 3:
+        return render(request, 'booot/index.html', {'images' : image})
+    elif way == 4:
+        return render(request, 'booot/index2.html')
 # Create your views here.
